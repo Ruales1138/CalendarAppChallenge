4f3acbc29c99752388ca2a2b1b001b7e52e8d525
luna1860 <saritaprinmc@gmail.com>
2024-09-12 17:23:20 -0500

    class Day

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -46,9 +46,55 @@ class Event:
         return f'ID: {self.id} Event title: {self.title} Description: {self.description} Time: {self.start_at} - {self.end_at}'
 
 
-
-
 # TODO: Implement Day class here
+class Day:
+    def __init__(self, date_: date):
+        self.date_: date = date_
+        self.slots: dict[time, str | None] = {}
 
+    def _init_slots(self):
+        pass
+
+    def add_event(self, event_id: str, start_at: time, end_at: time):
+        current_time = start_at
+        while current_time < end_at:
+            if self.slots.get(current_time) is not None:
+                slot_not_available_error()
+                return
+            current_time = self._increment_time(current_time)
+
+        current_time = start_at
+        while current_time < end_at:
+            self.slots[current_time] = event_id
+            current_time = self._increment_time(current_time)
+
+    def delete_event(self, event_id: str):
+        deleted = False
+        for slot, saved_id in self.slots.items():
+            if saved_id == event_id:
+                self.slots[slot] = None
+                deleted = True
+        if not deleted:
+            event_not_found_error()
+
+    def update_event(self, event_id: str, start_at: time, end_at: time):
+        for slot in self.slots:
+            if self.slots[slot] == event_id:
+                self.slots[slot] = None
+
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                if self.slots[slot]:
+                    slot_not_available_error()
+                else:
+                    self.slots[slot] = event_id
+
+    def _increment_time(self, t: time) -> time:
+        new_minute = t.minute + 15
+        new_hour = t.hour
+        if new_minute == 60:
+            new_minute = 0
+            new_hour += 1
+        return time(new_hour, new_minute)
 
 # TODO: Implement Calendar class here
