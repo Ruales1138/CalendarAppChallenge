9be544c1334c65c499ea750432a71c6f95bd5a7d
PC David <hruales1138@gmail.com>
2024-09-12 16:29:22 -0500

    clase Event terminada

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -23,12 +23,29 @@ class Reminder:
 # TODO: Implement Event class here
 @dataclass
 class Event:
-    def __init__(self, title: str, description: str, date_: date, start_at: time, end_at: time):
+    def __init__(self, title: str, description: str, date_: date, start_at: time, end_at: time, id: str = None):
         self.title: str = title
         self.description: str = description
         self.date_: date = date_
         self.start_at: time = start_at
         self.end_at: time = end_at
+        self.reminders: list[Reminder] = []
+        self.id: str = generate_unique_id()
+
+    def add_reminder(self, date_time: datetime, reminder_type: str = Reminder.EMAIL):
+        reminder = Reminder(date_time=date_time, type=reminder_type)
+        self.reminders.append(reminder)
+
+    def delete_reminder(self, reminder_index: int):
+        if 0 <= reminder_index < len(self.reminders):
+            del self.reminders[reminder_index]
+        else:
+            reminder_not_found_error()
+
+    def __str__(self) -> str:
+        return f'ID: {self.id} Event title: {self.title} Description: {self.description} Time: {self.start_at} - {self.end_at}'
+
+
 
 
 # TODO: Implement Day class here
